c29bdcc2956d0a087f3c5c61f769a9b0a789cc12
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-02-20 11:49:24 +1100

    Add ProductTag column definitions

--- models/Tag.js
+++ models/Tag.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
@@ -10,18 +10,18 @@ Tag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     tag_name: {
       type: DataTypes.STRING,
-    }
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
